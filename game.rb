df3501fefb845f9a8d072d75306f30da07a926f1
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-03-09 00:37:20 +0900

    add brave.rb

---
+++ game.rb
@@ -0,0 +1,25 @@
+class Brave
+    def initialize(hp:,attack:,defense:)
+        @hp = hp
+        @attack = attack
+        @defense = defense
+    end
+    def print
+        puts @hp ,@attack, @defense
+    end
+end
+
+class Monster
+    def initialize(hp:,attack:,defense:)
+        @hp = hp
+        @attack = attack
+        @defense = defense
+    end
+end
+
+class Battle
+    brave = Brave.new(hp:200,attack:20,defense:10)
+    monster = Monster.new(hp:300,attack:10,defense:20)
+    # p brave,monster
+    puts brave.print
+end
